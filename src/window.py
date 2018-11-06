1212f99e441e0e7d3b4e204cf5e17c26a681787a
Rohan Chacko <rohanchacko007@gmail.com>
2018-11-06 12:21:27 +0530

    committing changes

--- src/window.py
+++ src/window.py
@@ -1,4 +1,4 @@
-import cv2 
+import cv2
 import Config
 def localise_match(found, max_loc, templates, height, width, ratio):
     """ Calculates the bounding box for the region of interest """
@@ -41,24 +41,24 @@ def draw_match(frame, max_val, thresh_max,
     if max_of_all > thresh_max:
         is_drawn = True
         font = cv2.FONT_HERSHEY_SIMPLEX
-        # if number == 1:
-        #     cv2.rectangle(frame, (startx_coord[index_of_max], starty_coord[index_of_max]), (endx_coord[
-        #         index_of_max], endy_coord[index_of_max]), (0, 0, 255), 2)
-        #     cv2.putText(frame, articlename, (startx_coord[index_of_max], starty_coord[index_of_max] - 3),
-        #                 font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-        # else:
-        #     cv2.rectangle(frame, (startx_coord[index_of_max], starty_coord[index_of_max]), (endx_coord[
-        #         index_of_max], endy_coord[index_of_max]), (0, 255, 0), 2)
-        #     cv2.putText(frame, articlename, (startx_coord[index_of_max], starty_coord[index_of_max] - 3),
-        #                 font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+        if number == 1:
+            cv2.rectangle(frame, (startx_coord[index_of_max], starty_coord[index_of_max]), (endx_coord[
+                index_of_max], endy_coord[index_of_max]), (0, 0, 255), 2)
+            cv2.putText(frame, articlename, (startx_coord[index_of_max], starty_coord[index_of_max] - 3),
+                        font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+        else:
+            cv2.rectangle(frame, (startx_coord[index_of_max], starty_coord[index_of_max]), (endx_coord[
+                index_of_max], endy_coord[index_of_max]), (0, 255, 0), 2)
+            cv2.putText(frame, articlename, (startx_coord[index_of_max], starty_coord[index_of_max] - 3),
+                        font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
 
     Config.fps.stop()
-    # cv2.putText(frame, "Elapsed time: {:.2f}".format(Config.fps.elapsed()), Config.position_elapsed,
-    #             Config.font, Config.fontScale, Config.fontColor, Config.lineType)
-    # cv2.putText(frame, "FPS: {:.2f}".format(Config.fps.fps()),
-    #             Config.position_fps, Config.font, Config.fontScale,
-    #             Config.fontColor, Config.lineType)
+    cv2.putText(frame, "Elapsed time: {:.2f}".format(Config.fps.elapsed()), Config.position_elapsed,
+                Config.font, Config.fontScale, Config.fontColor, Config.lineType)
+    cv2.putText(frame, "FPS: {:.2f}".format(Config.fps.fps()),
+                Config.position_fps, Config.font, Config.fontScale,
+                Config.fontColor, Config.lineType)
 
-    # cv2.imshow("Result", frame)
+    cv2.imshow("Result", frame)
     print("\rFPS: {:.2f}".format(Config.fps.fps()), end='\r')
     return is_drawn, index_of_max, frame
