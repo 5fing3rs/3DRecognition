9b26e445e7529c06b7276616e1ab2a4cadaeaf64
priyankmodiPM <priyankmodi99@gmail.com>
2018-11-09 00:10:28 +0530

    Progress bar in template generator Added functionality to accept the base width as a parameter from the user

--- src/utilities.py
+++ src/utilities.py
@@ -31,3 +31,12 @@ def printProgressBar (fps,iteration, total, prefix = '', suffix = '', decimals =
     # Print New Line on Complete
     if iteration == total:
         print()
+
+def printProgressBar1 (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)    
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
